cd410478f99cba7baf299bfa83f0ffae05255cd0
Ole <oksandum@gmail.com>
2019-12-06 11:32:54 +0100

    Implement day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -11,6 +11,42 @@ pub const DAY2: Day = Day {
     },
 };
 
+struct Program(Vec<usize>);
+
+impl Program {
+    fn read(input: &mut dyn BufRead) -> Result<Program, Box<dyn Error>> {
+        let program: Result<_, Box<dyn Error>> = input
+            .split(b',')
+            .map(|r| {
+                Ok(String::from_utf8(r?)?.parse()?)
+            })
+            .collect();
+
+        Ok(Program(program?))
+    }
+
+    fn run(&self, noun: usize, verb: usize) -> usize {
+        let mut state = self.0.clone();
+        state[1] = noun;
+        state[2] = verb;
+
+        let mut instruction_pointer = 0;
+        let result =
+            loop {
+                let instruction = parse_instruction(&state[instruction_pointer..])
+                    .expect("Unable to parse instruction");
+                let halted = perform_instruction(&mut state, instruction);
+                if halted {
+                    break state[0];
+                }
+
+                instruction_pointer += 4;
+            };
+
+        result
+    }
+}
+
 enum Instruction {
     Add(usize, usize, usize),
     Mul(usize, usize, usize),
@@ -18,35 +54,30 @@ enum Instruction {
 }
 
 fn part1(input: &mut dyn BufRead) {
-    let mut program = read_program(input).expect("Unable to read program");
-    restore_program(&mut program);
-
-    let mut instruction_pointer = 0;
-    let result =
-        loop {
-            let instruction = parse_instruction(&program[instruction_pointer..])
-                .expect("Unable to parse instruction");
-            let halted = perform_instruction(&mut program, instruction);
-            if halted {
-                break program[0];
-            }
-
-            instruction_pointer += 4;
-        };
+    let program = Program::read(input).expect("Unable to read program");
+    let result = program.run(12, 2);
 
     println!("Result: {}", result);
 }
 
-fn part2(input: &mut dyn BufRead) {}
+fn part2(input: &mut dyn BufRead) {
+    const WANTED_OUTPUT: usize = 19690720;
+    let program = Program::read(input).expect("Unable to read program");
 
-fn read_program(input: &mut dyn BufRead) -> Result<Vec<usize>, Box<dyn Error>> {
-    input
-        .split(b',')
-        .map(|r| {
-            Ok(String::from_utf8(r?)?.parse()?)
-        })
-        .collect()
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let result =
+                program.run(noun, verb);
+
+            if result == WANTED_OUTPUT {
+                println!("Noun: {}, Verb: {}", noun, verb);
+                println!("Answer: {}", 100 * noun + verb);
+                break;
+            }
+        }
+    }
 }
+
 
 fn parse_instruction(slice: &[usize]) -> Result<Instruction, usize> {
     let opcode = slice[0];
@@ -66,9 +97,4 @@ fn perform_instruction(program: &mut [usize], instruction: Instruction) -> bool 
     }
 
     false
-}
-
-fn restore_program(program: &mut [usize]) {
-    program[1] = 12;
-    program[2] = 2;
 }
